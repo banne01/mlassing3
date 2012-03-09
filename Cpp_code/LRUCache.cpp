c212e17cc9a4732d2f335d685f1d4f05eb1ece9b
nehal <nehal@nehal-laptop.(none)>
2012-03-09 02:46:21 -0500

    lock

--- Cpp_code/LRUCache.cpp
+++ Cpp_code/LRUCache.cpp
@@ -41,6 +41,25 @@ class ObjCache{
         void insert(string & key,Objtype &nobj); 
         Objtype* find(string& key);
         int remove(const string &key );
+        void printcache(){
+        ListIter it = Clist.begin();
+        while(it != Clist.end()){
+         cout<<*it;
+         cout<<endl;
+         it++;
+        }
+        return ;
+        }
+        
+        void getlock(){
+        
+        }
+        void trylock(){
+        
+        }
+        void putlock(){
+        
+        }
     private :
         int usedslot;
         int maxslots;
@@ -53,7 +72,7 @@ template <class Objtype>
 void ObjCache<Objtype>::insert(string& key, Objtype& nobj){
     MapIter mitr = Cmap.find(key);    
     if(mitr != Cmap.end()){ // element present int the list 
-      ListIter litr = mitr->second();
+      ListIter litr = mitr->second;
       Clist.erase(litr);  // remove the element
       Cmap.erase(mitr);
       Clist.push_front(nobj); // push front
@@ -76,23 +95,53 @@ Objtype* ObjCache<Objtype>::find(string& key){
     MapIter mitr = Cmap.find(key);    
     if(mitr == Cmap.end())
         return NULL;
-    ListIter litr = mitr->second();
+    ListIter litr = mitr->second;
     Clist.erase(litr);  // remove the element
     Cmap.erase(mitr);
     Clist.push_front(*litr); // push front
     litr = Clist.begin(); // take the new iterator
     Cmap.insert(make_pair(key,litr)); // insert back in map
-    return *litr;
-
+    return &(*litr);
 }
 
 
 
 template <class Objtype>
 int ObjCache<Objtype>::remove(const string &key ){
+    MapIter mitr = Cmap.find(key);    
+    if(mitr == Cmap.end())
+        return 0;
+    ListIter litr = mitr->second;
+    Clist.erase(litr);  // remove the element
 
-
+return 1;
 }
 
+int main(){
+
+ ObjCache<string> lrucache(20); 
+
+ string s1("abc");
+ string s1k("12");
+ string s2k("344");
+ string s2("sdwewer");
+ string s3("dsfds");
+ string s3k("1244");
+ string s4k("23424");
+ string s4("sdfsdfs");
+   
+ lrucache.insert(s1,s1k);
+ lrucache.insert(s2,s2k);
+ lrucache.insert(s3,s3k);
+ lrucache.insert(s4,s4k);
+ lrucache.printcache();
+ 
+ string * temp = lrucache.find(s2);
+ temp->append("###"); 
+ lrucache.printcache();
+ 
+ lrucache.remove(s2);   
+ lrucache.printcache();
+}
 
 
